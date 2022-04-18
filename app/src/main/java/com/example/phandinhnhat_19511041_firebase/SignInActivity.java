70a4ee4874d99379164b7c73ae1bfa099598e0af
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-04-18 14:45:46 +0700

    create project

---
+++ app/src/main/java/com/example/phandinhnhat_19511041_firebase/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.phandinhnhat_19511041_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
